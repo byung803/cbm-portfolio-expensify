67de6a16297e99dc3df3f00321105fa506b2baf6
CHOI <byung803@gmail.com>
2018-01-26 15:26:50 +0900

    Setup expenseTotal and expenseTotalTest then render on ExpenseList

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux'; 
+import numeral from 'numeral';
 import ExpenseListItem from './ExpenseListItem';
 import selectExpenses from '../selectors/expenses'; 
+import selectExpensesTotal from '../selectors/expensesTotal';
 
 export const ExpenseList = (props) => (
     <div>
@@ -11,19 +13,21 @@ export const ExpenseList = (props) => (
             ) : (
                 props.expenses.map((expense, i) => {
                     return (
-                            <ExpenseListItem key={expense.id} expense={expense} /> 
+                            <ExpenseListItem key={expense.id} expense={expense} />                            
                     );
-                })
+                })                
             )
         }
+        <p>{`Viewing ${props.expenses.length} Totalling ${numeral(props.total / 100).format('$0,0.00')}`}</p>
     </div>
 );
 
 const mapStateToProps = (state) => {
     return {
-        expenses: selectExpenses(state.expenses, state.filters)
+        expenses: selectExpenses(state.expenses, state.filters),
+        total: selectExpensesTotal(selectExpenses(state.expenses, state.filters))
     };
-}
+};
 
 
 export default connect(mapStateToProps)(ExpenseList); 
