388c46e8039531eabd9d38521df596eb22bcc095
CHOI <byung803@gmail.com>
2018-01-26 10:34:33 +0900

    Setup devDependencies and dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = (env) => {
     return {
         entry: './src/app.js',
         output: {
-            path: path.join(__dirname, 'public'),
+            path: path.join(__dirname, 'public', 'dist'),
             filename: 'bundle.js'
         },
         module: {
@@ -48,7 +48,8 @@ module.exports = (env) => {
         devtool: isProduction ? 'source-map' : 'inline-source-map',
         devServer: {
             contentBase: path.join(__dirname, "public"),
-            historyApiFallback: true // 404 에러나면 index.html로 돌아오게함. 
+            historyApiFallback: true, // 404 에러나면 index.html로 돌아오게함. 
+            publicPath: '/dist/'
         }
     }
 };
