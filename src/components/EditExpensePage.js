7143aa00cc3e6379e5ca2f43df1c3e7ddccc8d67
BYUNGMIN CHOI <byung803@gmail.com>
2019-02-10 00:46:56 +0900

    react-modal removed

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,70 +1,65 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import ExpenseForm from './ExpenseForm';
-import { startEditExpense, startRemoveExpense } from '../actions/expenses'
-import RemoveModal from './RemoveModal';
+import React from "react";
+import { connect } from "react-redux";
+import ExpenseForm from "./ExpenseForm";
+import { startEditExpense, startRemoveExpense } from "../actions/expenses";
+import RemoveModal from "./RemoveModal";
 
 export class EditExpensePage extends React.Component {
-    state = {
-        modalToggler: false
-    }
+  state = {
+    modalToggler: false
+  };
 
-    onSubmit = (expense) => {
-        this.props.startEditExpense(this.props.expense.id, expense);
-        this.props.history.push('/');
-    }
+  onSubmit = expense => {
+    this.props.startEditExpense(this.props.expense.id, expense);
+    this.props.history.push("/");
+  };
 
-    openModal = () => {
-        this.setState(() => ({ modalToggler: true }));
-        // this.props.startRemoveExpense(this.props.expense);
-        // this.props.history.push('/');
-    }
+  openModal = () => {
+    this.setState(() => ({ modalToggler: true }));
+  };
 
-    closeModal = () => {
-        this.setState(() => ({ modalToggler: false }));
-    }
+  onRemove = () => {
+    this.props.startRemoveExpense(this.props.expense);
+    this.props.history.push("/");
+  };
 
-    render() {
-        return (
-            <div>
-                <div className="page-header">
-                    <div className="content-container">
-                        <h1 className="page-header__title">Edit Expense</h1>
-                    </div>
-                </div>
-                <div className="content-container">
-                    <ExpenseForm
-                        expense={this.props.expense}
-                        onSubmit={this.onSubmit}
-                    />
-                    <button
-                        className="button button--secondary"
-                        onClick={this.openModal}
-                    >
-                        Remove Expense
-                    </button>
-                    <RemoveModal
-                        modalToggler={this.state.modalToggler}
-                        closeModal={this.closeModal}
-                        expense={this.props.expense}
-                        history={this.props.history}
-                    />
-                </div>
-            </div>
-        );
-    }
+  closeModal = () => {
+    this.setState(() => ({ modalToggler: false }));
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="page-header">
+          <div className="content-container">
+            <h1 className="page-header__title">Edit Expense</h1>
+          </div>
+        </div>
+        <div className="content-container">
+          <ExpenseForm expense={this.props.expense} onSubmit={this.onSubmit} />
+          <button className="button button--secondary" onClick={this.onRemove}>
+            Remove Expense
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
-
 const mapStateToProps = (state, props) => {
-    return {
-        expense: state.expenses.find((expense) => expense.id === props.match.params.id)
-    };
+  return {
+    expense: state.expenses.find(
+      expense => expense.id === props.match.params.id
+    )
+  };
 };
 
-const mapDispatchToProps = (dispatch) => ({
-    startEditExpense: (id, expense) => dispatch(startEditExpense(id, expense)),
-    startRemoveExpense: (expense) => dispatch(startRemoveExpense(expense))
+const mapDispatchToProps = dispatch => ({
+  startEditExpense: (id, expense) => dispatch(startEditExpense(id, expense)),
+  startRemoveExpense: expense => dispatch(startRemoveExpense(expense))
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(EditExpensePage);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(EditExpensePage);
